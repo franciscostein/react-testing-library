a884a23b83caa7d164f14669d75039ba89dc13f2
Leandro Costa <leandro.franc2008@hotmail.com>
2021-06-22 16:52:01 -0300

    lesson 43

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
@@ -26,11 +26,21 @@ describe('checkbox', () => {
 });
 
 describe('popover', () => {
-    test('responds to hover', () => {
+    test('responds to hover', async () => {
+        render(<SummaryForm />);
+        const termsAndConditons = screen.getByText(/terms and conditions/i);
+
         // starts out hidden
+        const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+        expect(nullPopover).not.toBeInTheDocument();
 
         // appears upon mouseover of checkbox label
+        userEvent.hover(termsAndConditons);
+        const popover = screen.getByText(/no ice cream will actually be delivered/i);
+        expect(popover).toBeInTheDocument();
 
         // disappears when mouse out
+        userEvent.unhover(termsAndConditons);
+        await waitForElementToBeRemoved(() => screen.queryByText(/no ice cream will actually be delivered/i));
     });
 });

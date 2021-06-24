4bca2206a690d7f1d8569a876eb218232aaf615b
Leandro Costa <leandro.franc2008@hotmail.com>
2021-06-23 21:53:17 -0300

    lesson 64

--- sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -1,6 +1,5 @@
 import { render, screen } from '../../../test-utils/testing-library-utils';
 import Options from '../Options';
-import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
 
 test('displays image for each scoop option from server', async () => {
     render(<Options optionType="scoops" />);
--- sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,10 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../../test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
-import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
 
 test('update scoop subtotal when scoops change', async () => {
-    render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
+    render(<Options optionType="scoops" />);
 
     // make sure total starts out at $0.00
     const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false });
@@ -21,4 +20,22 @@ test('update scoop subtotal when scoops change', async () => {
     userEvent.clear(chocolateInput);
     userEvent.type(chocolateInput, '2');  // $2.00 each
     expect(scoopsSubtotal).toHaveTextContent('6.00');
+});
+
+test('check and uncheck toppings update subtotal', async () => {
+    render(<Options optionType="toppings" />);
+
+    const toppingsSubtotal = screen.getByText('Toppings total: $', { exact: false });
+    expect(toppingsSubtotal).toHaveTextContent('0.00');
+
+    const cherriesCheckbox = await screen.findByRole('checkbox', { name: /cherries/i })
+    userEvent.click(cherriesCheckbox);
+    expect(toppingsSubtotal).toHaveTextContent('1.50');
+
+    const hotFudgeCheckbox = await screen.findByRole('checkbox', { name: /hot fudge/i });
+    userEvent.click(hotFudgeCheckbox);
+    expect(toppingsSubtotal).toHaveTextContent('3.00');
+
+    userEvent.click(cherriesCheckbox);
+    expect(toppingsSubtotal).toHaveTextContent('1.50');
 });
